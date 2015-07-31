dfdb2885cefae0a2be83603b35d26ce45b626d0e
Greg Janesch <gjanesch@gmail.com>
2015-07-31 17:07:01 -0400

    Re-upload to Github

--- pmlPrediction.R
+++ pmlPrediction.R
@@ -1,14 +1,16 @@
+### Greg Janesch, last updated 01/24/2015
+### Description: function for predicting how a barbell lift was performed, given certain training data
+
+
 pmlPrediction <- function(){
     
     library(caret)
     
-    #set.seed(1701)
-    
-    # Read in the data set
+    ## Read in the data set
     training <- read.csv("pml-training.csv")
     
+    ## Convert non-numeric columns to numeric
     columnClasses <- sapply(training,class)
-    
     for(i in 8:159){
         if(columnClasses[i] != "numeric"){
             if(columnClasses[i] == "factor"){
@@ -23,35 +25,27 @@ pmlPrediction <- function(){
     NAcount <- sapply(training, function(x) sum(is.na(x)))
     training <- training[,ifelse(NAcount == 0, TRUE, FALSE)]
     
+    ## The first seven columns contain data that isn't relevant here
     training <- training[,-c(1:7)]
-    #training <- training[,-c(1,2,56)]
     
     avgright <- 0
     a <- 10
     
-    #for(i in 1:a){
-        
-        #print(paste("Now testing iteration number ", i, "...", sep = ""))
-        splitter <- createDataPartition(training$classe, p = 0.6, list = FALSE)
-        training_train <- training[splitter,]
-        training_test <- training[-splitter,]
-        
-        modFit <- train(data = training_train, classe ~ yaw_belt + accel_belt_z + gyros_arm_x + accel_arm_x + roll_dumbbell + gyros_dumbbell_x)
-        
-        pred <- predict(modFit, training_train)
-        print(paste("Fraction correct on training:", sum(pred == training_train$classe)/length(pred)))
-        
-        pred <- predict(modFit, training_test)
-        print(paste("Fraction correct on testing:", sum(pred == training_test$classe)/length(pred)))
-        
-        #avgright <- avgright + pred
-        
-    #}
+    ## Divide up the training data into a training subset and a test subset
+    splitter <- createDataPartition(training$classe, p = 0.6, list = FALSE)
+    training_train <- training[splitter,]
+    training_test <- training[-splitter,]
     
-    #avgright <- avgright/a
-    #print(paste("Average proportion correct:", avgright))
+    ## Train the model
+    modFit <- train(data = training_train, classe ~ yaw_belt + accel_belt_z + gyros_arm_x + accel_arm_x +
+                    roll_dumbbell + gyros_dumbbell_x)
+    
+    ## Make predictions and report back the accuracies of the model
+    pred_train <- predict(modFit, training_train)
+    print(paste("Fraction correct on training:", sum(pred_train == training_train$classe)/length(pred_train)))
+    pred_test <- predict(modFit, training_test)
+    print(paste("Fraction correct on testing:", sum(pred_test == training_test$classe)/length(pred_test)))
     
     return(training)
     
-    #modFit <- train(classe ~ yaw_belt + accel_belt_z + gyros_arm_x, data = training_train)
 }
